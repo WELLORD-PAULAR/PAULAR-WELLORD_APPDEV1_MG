0bd2c1706353311ba687225e50a7b859bf2003cf
WELLORD-PAULAR <20187455@s.ubaguio.edu>
2025-10-28 12:29:01 +0800

    AHHHHHHHHHHH

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { CityList } from './city-list/city-list';
 
 export const routes: Routes = [
   { path: '', component: Home },
+  { path: 'cities', component: CityList },
 ];
